bd5119384fedc6b90890c3b3ee4e672a4d06762e
Electropelmen <vitya.shevch.11.04@gmail.com>
2024-11-03 12:52:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
+# pyinstaller --add-data "images;./images" -F -w main.py
 import pygame
-from random import randint
-from time import time
 from scripts.player import Player
 from scripts.functions import load_image
 from scripts.bullet import Bullet
 from scripts.enemy import Enemy
-
+from time import time
+from random import randint
 pygame.init()
 
 
 flags = pygame.RESIZABLE | pygame.SCALED
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((800, 600), flags)
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 32)
-big_font = pygame.font.Font(None, 64)
-
+big_font = pygame.font.Font(None, 128)
 
 FPS = 60
 
 
-background = load_image('images','background.png', size=(800, 600), colorkey=None)
-bullet_image = load_image('images','bullet.png', size=(50, 40), colorkey=(255, 255, 255))
-enemy_image = load_image('images','enemy.png', size=(90, 90), colorkey=(0, 0, 0))
-player_image = load_image('images','player.png', size=(90, 90), colorkey=(255,255,255))
-player = Player(400, 550, player_image, 6, 3)
+background = load_image('images','background.png', size = (800, 600), colorkey = None)
+enemy_image = load_image('images','enemy.png', size = (90, 90), colorkey = (255, 255, 255))
+bullet_image = load_image('images','bullet.png', size = (40, 40), colorkey = (255, 255, 255))
+player_image = load_image('images','player.png', size = (80, 80), colorkey = (255, 255, 255))
+
+
+player_image = pygame.transform.scale(player_image, (75, 75))
+player = Player(400, 550, player_image, 5, 3)
 bullets = list()
 enemies = list()
-
-spawn_delta = 3.5
-timer = time()
 score = 0
+spawn_delta = 3
+timer = time()
+
+
 
 game = True
 died = False
 
+
 while game:
-    #Обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
@@ -50,66 +53,69 @@ while game:
             if event.key == pygame.K_SPACE and not died:
                 bullets.append(Bullet(player.rect.centerx, player.rect.y, bullet_image, 10))
 
-    #Обновление объектов
     
-
+    
     if not died:
         player.update()
         for bullet in bullets:
             bullet.update()
             if bullet.rect.bottom <= 0:
                 bullets.remove(bullet)
-        
-        delta = time() - timer
-        if delta >= spawn_delta:
-            timer = time()
-            x = randint(enemy_image.get_width()//2, 800 - enemy_image.get_width()//2)
-            y = enemy_image.get_height() // 2
-            speed = randint(5000, 7000)/1000
-            health = randint(1, 3)
-            enemies.append(Enemy(x, y, enemy_image, speed, health))
+
         for enemy in enemies:
-            enemy.update()
+            enemy.update() 
             for bullet in bullets:
                 if enemy.is_collide(bullet):
                     bullets.remove(bullet)
                     enemy.get_damage()
+
             if enemy.is_collide(player):
                 player.get_damage()
                 enemies.remove(enemy)
                 died = player.health <= 0
+
             elif enemy.health <= 0:
-                score += 1
                 enemies.remove(enemy)
+                score += 1
+
         
 
-    #Отриисовка объектов
-    window.blit(background, (0,0))
+
+        if time() - timer > spawn_delta:
+            timer = time()
+            x = randint(enemy_image.get_width() // 2, 800 - enemy_image.get_width() // 2)
+            y = - enemy_image.get_height() / 2
+            speed = randint(5000, 7000) / 1000
+            health = randint(1, 3)
+            enemies.append(Enemy(x, y, enemy_image, speed, health))
+    
+
+
+
+    window.blit(background, (0, 0))
     player.render(window)
     for enemy in enemies:
         enemy.render(window)
     for bullet in bullets:
         bullet.render(window)
-    
-    text = 'Очки: '+ str (score)    
+    text = 'Очки: ' + str(score)
     image_text = font.render(text, True, (255,255,255))
     image_rect = image_text.get_rect(midtop = (400, 0))
     window.blit(image_text, image_rect)
 
-    text_hp = 'Жизни: '+ str (player.health)    
-    image_text_hp = font.render(text_hp, True, (255,255,255))
-    image_rect_hp = image_text.get_rect(topleft = (0, 0))
-    window.blit(image_text_hp, image_rect_hp)
+    text = 'Жизни: ' + str(player.health)
+    image_text = font.render(text, True, (255,255,255))
+    image_rect = image_text.get_rect(topleft = (0, 0))
+    window.blit(image_text, image_rect)
     if died:
-        text_die = 'YOU DIED!!!'
-        image_text_die = big_font.render(text_die, True, (255, 50, 50))
-        image_rect_die = image_text_die.get_rect(center = (400, 300))
-        window.blit(image_text_die, image_rect_die)
+        text = 'YOU DIED!!!'
+        image_text = big_font.render(text, True, (255, 50, 50))
+        image_rect = image_text.get_rect(center = (400, 300))
+        window.blit(image_text, image_rect)
+        text = 'Нажмите любую клавишу, чтобы начать заново'
+        image_text = font.render(text, True, (255, 255, 255))
+        image_rect = image_text.get_rect(center = (400, 400))
+        window.blit(image_text, image_rect)
 
-        text_die = 'Нажмите любую клавишу, чтобы начать заново'
-        image_text_die = font.render(text_die, True, (255, 255, 255))
-        image_rect_die = image_text_die.get_rect(center = (400, 400))
-        window.blit(image_text_die, image_rect_die)        
     pygame.display.update()
-    clock.tick(60)
-    
+    clock.tick(FPS)
--- scripts/functions.py
+++ scripts/functions.py
@@ -1,17 +1,16 @@
 import pygame
 import os
 import sys
+
 def load_image(*paths, size, colorkey):
-    'path: путь к картинке'
-    'size: размер картинки'
-    'colorkey: цвет, который отвечает за прозрачность'
     path = get_path(*paths)
     image = pygame.image.load(path).convert()
     image = pygame.transform.scale(image, size)
     image.set_colorkey(colorkey)
     return image
 
-base_path = getattr(sys, '_MEYPASS', os.path.abspath(os.path.dirname('main.py')))
+base_path = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname('main.py')))
+
 def get_path(*paths):
-    path = os.path.join(base_path , *paths)
+    path = os.path.join(base_path, *paths)
     return path
